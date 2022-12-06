da81b9cbef9a2f2b4ba784917779c804a3b364a3
ohjihye <jihye9099@gmail.com>
2022-12-06 23:44:43 +0900

    2차원 배열 작업중

---
+++ 2차원배열/Q2564.py
@@ -0,0 +1,31 @@
+# 구간이 겹쳤을 경우 - 를 하는 문제
+# 구간이 겹치지 않으면 면적은 100
+# 10에서 빠진 구간 만큼의 넓이를 빼준다
+# 3
+# x:3  y:7 
+# x:15 y:7 
+# x:5  y:2 
+# 각각의 구간을 가지고있는다
+# 구간들이 겹치는지 검증
+# 겹친다면 얼마나 겹치는지
+# 전체 종의 면적 - 겹치는 부분
+
+# caseLen = int(input())
+# colorPapers = []
+# for i in range(len(caseLen)):
+#     position = list(map(int,input().split(" ")))
+#     square = \
+# [position[0],position[0]+10],[position[1],position[1]+10]]
+#     colorPapers.append(square)
+
+colorPapers = [
+    [[3,13],[7,17]],
+    [[15,25],[7,17]],
+    [[5,15],[2,12]]
+]
+
+for i in range(len(colorPapers)):
+    for j in range(1, len(colorPapers)):
+        print(colorPapers[i][0],colorPapers[j][0])
+        print(colorPapers[i][1],colorPapers[j][1])
+    print()
--- 2차원배열/Q2738.py
+++ 2차원배열/Q2738.py
@@ -1,35 +1,19 @@
-# 3 3
-# 1 1 1
-# 2 2 2
-# 0 1 0
-# 3 3 3
-# 4 4 4
-# 5 5 100
-
 x, y = list(map(int, input().split(" ")))
 
 
-def make_metrix(width, height):
+def make_metrix(height):
     matrix = []
     for _ in range(height):
         matrix.append(list(map(int,input().split(" "))))
+        
     return matrix
 
 
-matrix_arr = [
-                [
-                    [1,1,1],
-                    [2,2,2],
-                    [0,1,0]
-                ],
-                [
-                    [3,3,3],
-                    [4,4,4],
-                    [5,5,100]
-                ]
-            ]
-for i in range(2):
-    matrix_arr.append(make_metrix(x, y))
+matrix_arr = []
+matrix_arr.append(make_metrix(x))
+matrix_arr.append(make_metrix(x))
 
-print(matrix_arr[0][0][0])
-print(matrix_arr[1][0][0])
+for i in range(x):
+    for j in range(y):
+        print(matrix_arr[0][i][j] + matrix_arr[1][i][j], end= " ")
+    print()
