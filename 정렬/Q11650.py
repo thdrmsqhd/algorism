3b148326c38f58d71fa3897920a209b4265593c6
송근봉 <thdrmsqhd@gmail.com>
2022-12-14 17:36:01 +0900

    Q11650 틀렸습니다.

--- 정렬/Q11650.py
+++ 정렬/Q11650.py
@@ -1,27 +1,8 @@
-# 문제
-# 2차원 평면 위의 점 N개가 주어진다. 좌표를 x좌표가 증가하는 순으로, x좌표가 같으면 y좌표가 증가하는 순서로 정렬한 다음 출력하는 프로그램을 작성하시오.
-#
-# 입력
-# 첫째 줄에 점의 개수 N (1 ≤ N ≤ 100,000)이 주어진다. 둘째 줄부터 N개의 줄에는 i번점의 위치 xi와 yi가 주어진다. (-100,000 ≤ xi, yi ≤ 100,000) 좌표는 항상 정수이고, 위치가 같은 두 점은 없다.
-#
-# 출력
-# 첫째 줄부터 N개의 줄에 점을 정렬한 결과를 출력한다.
-
-# 5
-# 3 4
-# 1 1
-# 1 -1
-# 2 2
-# 3 3
-
-targetLen = int(input())
-# targetLen = 5
-testCases = []
-for i in range(targetLen):
-    testCases.append(list(map(int,input().split(' '))))
-# testCases = [[3,4],[1,1],[1,-1],[2,2],[3,3]]
-
-testCases.sort(key=lambda x: (x[0], x[1]))
-
-for case in testCases:
-    print(case)
+import sys
+n = int(sys.stdin.readline())
+so = []
+for i in range(n):
+    so.append(list(map(int, sys.stdin.readline().split())))
+so.sort(key=lambda x: (x[0], x[1]))
+for i in so:
+    print(i[0], i[1])
